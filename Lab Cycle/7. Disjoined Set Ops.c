6be514ed3f791132b1dccbe5a46d59af5e25aef7
anlinalbert <anlinalbert@gmail.com>
2021-03-08 17:19:22 +0530

    Both lab programs 7 & 9 were wrong, replaced with correct pgms.

--- Lab Cycle/7. Disjoined Set Ops.c
+++ Lab Cycle/7. Disjoined Set Ops.c
@@ -1,201 +1,87 @@
-#include<stdio.h>  
-#include<conio.h>  
+#include<stdio.h>
 #include<stdlib.h>
 
-int unions(int[],int[],int[],int,int);  
-int input(int[]);  
-int intersection(int[],int[],int[],int,int);  
-int difference(int[],int[],int[],int,int);  
-void display(int[],int);  
-int find(int[], int[], int, int, int);
+int ar[10],a,b,i,j;
 
-void main() {  
-    int set1[20], set2[20], set3[40], s1=0, s2=0, s3, ch, ch1, ch2, a, b; 
-    int n, choice = 1;
-
-    while(choice != 0) {
-        printf("\n============================================\n");
-        printf("               DISJOINED SET OPS              \n");
-        printf("============================================\n");
-        printf("1. Input\n");
-        printf("2. Union\n");
-        printf("3. Intersection\n");
-        printf("4. Difference\n");
-        printf("5. Find\n");
-        printf("6. Display\n");
-        printf("0. Exit\n");
-        printf("--------------------------------------------\n");
-        printf("Enter your choice : ");
-
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 0 : break;
-            
-            case 1 :
-                printf("Enter set 1:\n");
-                s1 = input(set1);
-                printf("\nEnter set 2:\n");
-                s2 = input(set2);
-                break;
-
-            case 2 :
-                if(s1==0 &&s2==0)  
-                    printf("\n\n\t\tU HAVE TO ENTER SET FIRST"); 
-                else {
-                    s3 = unions(set1, set2, set3, s1, s2);
-                    display(set3,s3);
-                }
-                break;
-
-            case 3 :
-                if(s1==0 &&s2==0)  
-                    printf("\n\n\t\tU HAVE TO ENTER SET FIRST");  
-                else {  
-                    s3=intersection(set1,set2,set3,s1,s2);  
-                    display(set3,s3);  
-                }  
-                break;  
-
-            case 4 :
-                if(s1==0 &&s2==0)  
-                    printf("\n\n\t\tU HAVE TO ENTER SET FIRST"); 
-                else {
-                    printf("\tA - B\n");
-                    s3=difference(set1,set2,set3,s1,s2);  
-                    display(set3,s3);   
-                    printf("\n\n\tB - A\n");
-                    s3=difference(set2,set1,set3,s2,s1);  
-                    display(set3,s3);  
-                }
-                break;
-
-            case 5 :
-                if(s1==0 &&s2==0)  
-                    printf("\n\n\t\tU HAVE TO ENTER SET FIRST"); 
-                else {
-                    printf("Enter the first element: ");
-                    scanf("%d", &a);
-                    printf("Enter the second element: ");
-                    scanf("%d", &b);
-                    if(find(set1, set2, s1, s2, a) == find(set1, set2, s1, s2, b))
-                        printf("\nThe elements are in the same set.\n");
-                    else
-                        printf("\nThe elements are not in the same set.\n");
-                }
-                break;  
-            
-            case 6 :
-                if(s1==0 &&s2==0)  
-                    printf("\n\n\t\tU HAVE TO ENTER SET FIRST"); 
-                else {
-                    display(set1,s1);
-                    display(set2,s2);
-                }
-                break;
-
-            default:
-                printf("Enter a valid input");
-        }
-    }
+void create() {
+    for(i = 0; i < 9; i++)
+        ar[i] = i;
 }
 
+void display() {
+    printf("\nData Elements:\n\n");
+    for(i = 0; i < 9; i++)
+        printf("\t%d[%d]", i, ar[i]);
+    printf("\n");
+}
 
-/*****************************************************  
-                    INPUT FUNCTION  
-*****************************************************/  
-int input(int set[])  {  
-    int i=0,j,s;  
-    printf("\nENTER THE SIZE OF THE SET : ");  
-    scanf("%d",&s);  
-    printf("\nENTER THE SET\n");  
-    while(i<s) {  
-        scanf("%d",&set[i]);  
-        for(j=0;j<i;j++) {
-            if(set[i]==set[j])  {  
-                printf("\n\n!!!! DUPLICATED ENTRY !!!!\n\n");  
-                printf("----------: ENTER AGAIN");  
-                break;  
-            } 
-        } 
-        if(i==j)  
-            i++;  
-    }  
-    return s;  
-}  
+void unionop() {
+    int t,m;
 
-/*********************************************************  
-                    UNION FUNCTION  
-*********************************************************/  
-int unions(int set1[],int set2[],int set3[],int s1,int s2) {  
-    int s3,i,j;  
-    for(i=0;i<s1;i++)  
-        set3[i]=set1[i];  
-    s3=i;  
-    for(i=0;i<s2;i++) {  
-        for(j=0;j<s1;j++) {
-            if(set1[j]==set2[i])  
-            break;  
+    while(1) {
+        a = 0;
+        b = 0;
+
+        printf("\nEnter the parent: ");
+        scanf("%d",&b);
+        printf("Enter the element: ");
+        scanf("%d",&a);
+
+        if((a >= 0) && (a < 9) && (b >= 0) && (b < 9))
+           break;
+        else
+            printf("Invalid data");
+
+    }
+
+    int temp = ar[a];
+    for(i = 0; i < 9; i++)
+        if(ar[i] == temp)
+            ar[i] = ar[b];
+
+    display();
+}
+
+void find() {
+    while(1) {
+        a = 0;
+        b = 0;
+        printf("\nEnter the element 1: ");
+        scanf("%d",&a);
+        printf("Enter the element 2: ");
+        scanf("%d",&b);
+
+        if((a >= 0) && (a < 9) && (b >= 0) && (b < 9))
+           break;
+        else
+            printf("Result: Invalid data\n");
+    }
+
+    if(ar[a] == ar[b])
+        printf("Result: True\n");
+    else
+        printf("Result: Absent\n");
+}
+
+void main() {
+    int ch;
+
+    create();
+    display();
+
+    while(1) {
+        printf("\n1.Union\n2.Find\n3.Display\n4.Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d",&ch);
+        switch(ch) {
+            case 1: unionop();
+                break;
+            case 2: find();
+                break;
+            case 3: display();
+                break;
+            case 4: exit(0);
+            default : printf("\nInvalid input - try again.\n");
         }
-        if(j==s1)  
-            set3[s3++]=set2[i];  
-    }  
-    return s3;  
-}  
-
-/*************************************************************  
-                INTERSECTION FUNCTION  
-***************************************************************/  
-int intersection(int set1[],int set2[],int set3[],int s1,int s2) {  
-    int i,j,s3=0;  
-    for(i=0;i<s1;i++) {  
-        for(j=0;j<s2;j++)  
-            if(set1[i]==set2[j]) {  
-                set3[s3++]=set1[i];  
-                break;  
-            } 
-    }  
-    return s3;  
-}  
-
-/****************************************************************  
-                    DIFFERENCE FUNCTION  
-******************************************************************/  
-int difference(int set1[],int set2[],int set3[],int s1,int s2) {  
-    int i,j,s3=0;  
-    for(i=0;i<s1;i++) {  
-        for(j=0;j<s2;j++)  
-            if(set1[i]==set2[j])  
-                break;  
-            if(j==s2)  
-            set3[s3++]=set1[i];  
-    }  
-    return s3;  
-}  
-
-/*********************************************************  
-                    DISPLAY FUNCTION  
-**********************************************************/  
-void display(int set[],int size) {  
-    int i;  
-    printf("\n\n\t{ ");  
-    for(i=0;i<size;i++)  
-        printf("\t%d",set[i]);  
-    printf("\t} ");  
-    getch();  
-}  
-
-
-int find(int set1[], int set2[], int s1, int s2, int x) {
-    int c = 0;
-
-    for(int i = 0; i < s1; i++)
-        if(set1[i] == x)
-            c = 1;
-    
-    for(int i = 0; i < s2; i++)
-        if(set2[i] == x)
-            c = 2;
-
-    return c;
+    }
 }
